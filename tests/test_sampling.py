a4d4f0a547719895ef6c9afc6e8833e5039c3e6d
Emily E Meyer <emeyer121@gmail.com>
2026-06-23 16:05:18 -0400

    turned repetitive x into x_eval fixture

--- tests/test_sampling.py
+++ tests/test_sampling.py
@@ -13,35 +13,39 @@ mpl.use("agg")
 
 
 class TestSampling:
+    @pytest.fixture(scope="class")
+    def x_eval(self):
+        return torch.linspace(-5, 5, 101)
+
     @pytest.mark.parametrize("val_samp", [0.5, 1, 2])
-    def test_check_val_sampling(self, val_samp):
-        x = torch.linspace(-5, 5, 101)
+    def test_check_val_sampling(self, val_samp, x_eval):
         sampled, full, interps, coeffs, residuals = pooling.sampling.check_sampling(
-            val_sampling=val_samp, pix_sampling=None, func=pooling.pooling.gaussian, x=x
+            val_sampling=val_samp,
+            pix_sampling=None,
+            func=pooling.pooling.gaussian,
+            x=x_eval,
         )
         pooling.sampling.plot_coeffs(coeffs[:20], 10)
-        pooling.sampling.interpolation_plot(interps, residuals, None, val_samp, x)
-        pooling.sampling.create_movie(interps, residuals, x, full=full)
+        pooling.sampling.interpolation_plot(interps, residuals, None, val_samp, x_eval)
+        pooling.sampling.create_movie(interps, residuals, x_eval, full=full)
 
     @pytest.mark.parametrize("pix_samp", [2, 10, 25])
-    def test_check_pix_sampling(self, pix_samp):
-        x = torch.linspace(-5, 5, 101)
+    def test_check_pix_sampling(self, pix_samp, x_eval):
         sampled, full, interps, coeffs, residuals = pooling.sampling.check_sampling(
             val_sampling=None,
             pix_sampling=pix_samp,
             func=pooling.pooling.mother_window,
-            x=x,
+            x=x_eval,
         )
         pooling.sampling.plot_coeffs(coeffs[:20], 10)
-        pooling.sampling.interpolation_plot(interps, residuals, pix_samp, None, x)
-        pooling.sampling.create_movie(interps, residuals, x, full=full)
+        pooling.sampling.interpolation_plot(interps, residuals, pix_samp, None, x_eval)
+        pooling.sampling.create_movie(interps, residuals, x_eval, full=full)
 
-    def test_check_sampling_error(self):
-        x = torch.linspace(-5, 5, 101)
+    def test_check_sampling_error(self, x_eval):
         with pytest.raises(Exception):
             pooling.sampling.check_sampling(
                 val_sampling=2,
                 pix_sampling=2,
                 func=pooling.pooling.mother_window,
-                x=x,
+                x=x_eval,
             )
