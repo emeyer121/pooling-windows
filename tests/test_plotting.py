71785bcc638dbf621eecb0b51addbfcb965f289b
Emily E Meyer <emeyer121@gmail.com>
2026-06-23 15:58:08 -0400

    add parameterized variables instead of for loops

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -19,27 +19,26 @@ def close_figures_on_teardown():
 
 
 class TestPlotting:
-    def test_plotting(self, pool_win):
+    @pytest.mark.parametrize("scale_num", [0, 1])
+    def test_plotting(self, pool_win, scale_num):
         pool_win.plot_window_areas()
         pool_win.plot_window_widths()
-        for i in range(2):
-            pool_win.plot_window_areas("pixels", i)
-            pool_win.plot_window_widths("pixels", i)
+        pool_win.plot_window_areas("pixels", scale_num)
 
-    def test_plotting_windows(self, pool_win):
+    @pytest.mark.parametrize("windows_scale", [0, 1])
+    def test_plotting_windows(self, pool_win, windows_scale):
         pool_win.plot_windows()
-        for i in range(2):
-            pool_win.plot_windows(
-                contour_levels=0, colors="b", subset=False, windows_scale=i
-            )
+        pool_win.plot_windows(
+            contour_levels=0, colors="b", subset=False, windows_scale=windows_scale
+        )
 
-    def test_plotting_window_values(self, pool_win, torch_img):
+    @pytest.mark.parametrize("windows_scale", [0, 1])
+    def test_plotting_window_values(self, pool_win, torch_img, windows_scale):
         pool_win.plot_window_values()
         fig, axes = plt.subplots(1, 1, figsize=(4, 4))
         plt.imshow(torch_img.squeeze(), cmap="Greys_r", interpolation="none")
         pool_win.plot_window_values(im=torch_img, ax=axes, subset=False)
-        for i in range(2):
-            pool_win.plot_window_values(windows_scale=i)
+        pool_win.plot_window_values(windows_scale=windows_scale)
 
     def test_plot_angle_ecc(self):
         angle_w, ecc_w = pooling.pooling.create_pooling_windows(0.87, (256, 256))
--- tests/test_sampling.py
+++ tests/test_sampling.py
@@ -13,36 +13,35 @@ mpl.use("agg")
 
 
 class TestSampling:
-    def test_check_val_sampling(self):
+    @pytest.mark.parametrize("val_samp", [0.5, 1, 2])
+    def test_check_val_sampling(self, val_samp):
         x = torch.linspace(-5, 5, 101)
-        for samp in [0.5, 1, 2]:
-            sampled, full, interps, coeffs, residuals = pooling.sampling.check_sampling(
-                val_sampling=samp, pix_sampling=None, func=pooling.pooling.gaussian, x=x
-            )
-            pooling.sampling.plot_coeffs(coeffs[:20], 10)
-            pooling.sampling.interpolation_plot(interps, residuals, None, samp, x)
-            pooling.sampling.create_movie(interps, residuals, x, full=full)
+        sampled, full, interps, coeffs, residuals = pooling.sampling.check_sampling(
+            val_sampling=val_samp, pix_sampling=None, func=pooling.pooling.gaussian, x=x
+        )
+        pooling.sampling.plot_coeffs(coeffs[:20], 10)
+        pooling.sampling.interpolation_plot(interps, residuals, None, val_samp, x)
+        pooling.sampling.create_movie(interps, residuals, x, full=full)
 
-    def test_check_pix_sampling(self):
+    @pytest.mark.parametrize("pix_samp", [2, 10, 25])
+    def test_check_pix_sampling(self, pix_samp):
         x = torch.linspace(-5, 5, 101)
-        for samp in [2, 10, 25]:
-            sampled, full, interps, coeffs, residuals = pooling.sampling.check_sampling(
-                val_sampling=None,
-                pix_sampling=samp,
-                func=pooling.pooling.mother_window,
-                x=x,
-            )
-            pooling.sampling.plot_coeffs(coeffs[:20], 10)
-            pooling.sampling.interpolation_plot(interps, residuals, samp, None, x)
-            pooling.sampling.create_movie(interps, residuals, x, full=full)
+        sampled, full, interps, coeffs, residuals = pooling.sampling.check_sampling(
+            val_sampling=None,
+            pix_sampling=pix_samp,
+            func=pooling.pooling.mother_window,
+            x=x,
+        )
+        pooling.sampling.plot_coeffs(coeffs[:20], 10)
+        pooling.sampling.interpolation_plot(interps, residuals, pix_samp, None, x)
+        pooling.sampling.create_movie(interps, residuals, x, full=full)
 
     def test_check_sampling_error(self):
         x = torch.linspace(-5, 5, 101)
-        samp = 2
         with pytest.raises(Exception):
             pooling.sampling.check_sampling(
-                val_sampling=samp,
-                pix_sampling=samp,
+                val_sampling=2,
+                pix_sampling=2,
                 func=pooling.pooling.mother_window,
                 x=x,
             )
