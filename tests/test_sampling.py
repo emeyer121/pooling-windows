369a64bd3115b86259a848c4dcec0acdf2b1259d
Emily E Meyer <emeyer121@gmail.com>
2026-06-25 17:05:53 -0400

    edited sampling tests

--- tests/test_sampling.py
+++ tests/test_sampling.py
@@ -20,36 +20,31 @@ class TestSampling:
 
     @pytest.mark.parametrize("val_samp", [0.5, 1, 2])
     def test_check_val_sampling(self, val_samp, x_eval):
-        sampled, full, interps, coeffs, residuals = pooling.sampling.check_sampling(
+        pooling.sampling.check_sampling(
             val_sampling=val_samp,
             pix_sampling=None,
             func=pooling.pooling.gaussian,
             x=x_eval,
         )
-        pooling.sampling.plot_coeffs(coeffs[:20], 10)
-        pooling.sampling.interpolation_plot(interps, residuals, None, val_samp, x_eval)
-        pooling.sampling.create_movie(interps, residuals, x_eval, full=full)
 
     @pytest.mark.parametrize("pix_samp", [2, 10, 25])
     def test_check_pix_sampling(self, pix_samp, x_eval):
-        sampled, full, interps, coeffs, residuals = pooling.sampling.check_sampling(
+        pooling.sampling.check_sampling(
             val_sampling=None,
             pix_sampling=pix_samp,
             func=pooling.pooling.mother_window,
             x=x_eval,
         )
-        pooling.sampling.plot_coeffs(coeffs[:20], 10)
-        pooling.sampling.interpolation_plot(interps, residuals, pix_samp, None, x_eval)
-        pooling.sampling.create_movie(interps, residuals, x_eval, full=full)
 
     def test_check_sampling_error(self, x_eval):
-        with pytest.raises(Exception):
+        with pytest.raises(Exception) as excinfo:
             pooling.sampling.check_sampling(
                 val_sampling=2,
                 pix_sampling=2,
                 func=pooling.pooling.mother_window,
                 x=x_eval,
             )
+        assert "One of val_sampling or pix_sampling must be None!" in str(excinfo)
 
     def test_check_small_residuals(self, x_eval):
         _, _, _, _, residuals = pooling.sampling.check_sampling(0.5, x=x_eval)
@@ -71,7 +66,15 @@ class TestSampling:
         cent = np.argmin(abs(x_eval - 0))
         assert not np.allclose(interps[:, cent], np.array(orig_fun))
 
-    def test_check_interp_raisedcos(self, x_eval):
+    def test_check_bad_interp_avgdiff(self, x_eval):
+        _, _, interps, _, _ = pooling.sampling.check_sampling(
+            2, func=pooling.pooling.gaussian, x=x_eval
+        )
+        orig_fun = pooling.pooling.gaussian(x_eval)
+        cent = np.argmin(abs(x_eval - 0))
+        assert np.mean(np.abs(interps[:, cent] - np.array(orig_fun))) > 0.001
+
+    def test_check_interp_crossfunction(self, x_eval):
         _, _, interps, _, _ = pooling.sampling.check_sampling(
             0.5, func=pooling.pooling.gaussian, x=x_eval
         )
