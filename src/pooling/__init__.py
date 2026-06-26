d99c195542ce9c8b705c1f10102383f564f3cdf5
Emily E Meyer <emeyer121@gmail.com>
2026-06-26 09:39:46 -0400

    adding __dir__ to __init__.py

--- src/pooling/__init__.py
+++ src/pooling/__init__.py
@@ -4,3 +4,7 @@ __all__ = ["pooling_windows", "pooling", "sampling", "utils"]
 
 from . import pooling, sampling, utils
 from .pooling_windows import PoolingWindows as PoolingWindows
+
+
+def __dir__() -> list[str]:
+    return __all__
