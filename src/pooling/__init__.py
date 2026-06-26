54374c6e4315f683f80eca8a856144996a510b6e
Emily E Meyer <emeyer121@gmail.com>
2026-06-26 09:53:39 -0400

    in progress getting version to work

--- src/pooling/__init__.py
+++ src/pooling/__init__.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-__all__ = ["pooling_windows", "pooling", "sampling", "utils"]
+__all__ = ["__version__", "pooling_windows", "pooling", "sampling", "utils"]
 
 from . import pooling, sampling, utils
 from .pooling_windows import PoolingWindows as PoolingWindows
+from .version import __version__
 
 
 def __dir__() -> list[str]:
---
+++ src/pooling/version.py
@@ -0,0 +1,10 @@
+"""Pooling windows version information."""
+
+__all__ = ["__version__"]
+
+
+import contextlib
+from importlib.metadata import PackageNotFoundError, version
+
+with contextlib.suppress(PackageNotFoundError):
+    __version__ = version("pooling")
