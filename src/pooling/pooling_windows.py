d3a2037233c5d01132f3545812300d73bb8a5a00
Emily E Meyer <emeyer121@gmail.com>
2026-07-20 17:31:03 -0400

    add more info about window normalization

--- src/pooling/pooling_windows.py
+++ src/pooling/pooling_windows.py
@@ -249,7 +249,10 @@ class PoolingWindows(nn.Module):
             device="cpu"
         )
 
-    To normalize resulting windows:
+    To normalize resulting windows, so that they have an L1-norm of 1. This
+    is useful when generating model metamers using the windows' representation
+    so that each eccentricity contributes equally, facilitating optimization.
+    See [2]_ for an example:
 
     >>> ecc_windows, scale_factor = pooling.pooling.normalize_windows(
             angle_windows=angle_w,
@@ -263,6 +266,9 @@ class PoolingWindows(nn.Module):
     .. [1] Freeman, J., & Simoncelli, E. P. (2011). Metamers of the
        ventral stream. Nature Neuroscience, 14(9),
        1195–1201. http://dx.doi.org/10.1038/nn.2889
+    .. [2] Broderick, W. F., Rufo, G., Winawer, J. & Simoncelli, E. P.
+       (2023). Foveated metamers of the early visual system. eLife,
+       12:RP90554. http://dx.doi.org/10.7554/eLife.90554.2
 
     """
 
